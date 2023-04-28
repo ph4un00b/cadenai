903401fabedcebca8bd13db9162dc41dca241faa
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-04-28 14:43:19 -0600

    chore

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import { signIn, signOut } from "next-auth/react";

27d6e87734a02353a3d259eb3d43c161b0249c6b
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-05-11 14:32:05 -0600

    chore

--- apps/chat-models/src/server/index.ts
+++ apps/chat-models/src/server/index.ts
@@ -2,11 +2,14 @@
  * 2. initialize our main router instance
  * @see https://trpc.io/docs/quickstart#2-add-a-query-procedure
  */
+
+import { z } from "zod";
+
 import { publicProcedure, router } from "./trpc";
 
 const appRouter = router({
-	alo: publicProcedure.query(() => {
-		return "alo jamon!🎈";
+	alo: publicProcedure.input(z.string()).query(({ input }) => {
+		return "alo jamon! 🎈 " + input;
 	}),
 });
 // Export type router type signature,

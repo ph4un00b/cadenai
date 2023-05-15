e83be835143dacae1429a2556375ed847c119128
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-05-15 13:24:02 -0600

    loading from server

--- apps/embeddings-zact/src/app/(landing)/page.tsx
+++ apps/embeddings-zact/src/app/(landing)/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-	return <>cadenai</>;
+async function wait(ms: number) {
+	return new Promise((resolve) => {
+		globalThis.setTimeout(() => resolve("ok"), ms);
+	});
+}
+
+export default async function Home() {
+	await wait(3000);
+	return (
+		<div>
+			<h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+				<span className="text-pink-500">CadenAI 🤗!</span>
+			</h1>
+
+			<button className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+				wait!
+			</button>
+		</div>
+	);
 }

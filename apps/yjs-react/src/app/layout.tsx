ed68544e87c851ceea30cb35581d376d744f708b
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-09-14 18:16:35 -0600

    look and feel

--- apps/yjs-react/src/app/layout.tsx
+++ apps/yjs-react/src/app/layout.tsx
@@ -1,22 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import Sidebar from '@/components/Sidebar'
+import "./globals.css";
+import type { Metadata } from "next";
+
+import Sidebar from "~/components/Sidebar";
 
 export const metadata: Metadata = {
-  title: 'y-sweet demos',
-  description: 'Demos of y-sweet.',
-}
+	title: "y-sweet demos",
+	description: "Demos of y-sweet.",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="bg-[radial-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-pink-50 to-pink-900">
-        <Sidebar>
-          <div className="absolute w-full h-full top-0 pt-14 lg:relative lg:w-auto lg:pt-0 bg-[radial-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-white/90 via-pink-50/90 to-pink-100/90 sm:mr-2 sm:rounded-lg">
-            {children}
-          </div>
-        </Sidebar>
-      </body>
-    </html>
-  )
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className="bg-[radial-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-emerald-50 to-emerald-900">
+				<Sidebar>
+					<div className="absolute top-0 h-full w-full bg-[radial-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-white/10 via-emerald-400/90 to-emerald-800/90 pt-14 sm:mr-2 sm:rounded-lg lg:relative lg:w-auto lg:pt-0">
+						{children}
+					</div>
+				</Sidebar>
+			</body>
+		</html>
+	);
 }
--- apps/yjs-react/src/app/page.tsx
+++ apps/yjs-react/src/app/page.tsx
@@ -1,63 +1,66 @@
-import Link from 'next/link'
-import Title from '@/components/Title'
+import Link from "next/link";
+
+import Title from "~/components/Title";
 
 type HomeProps = {
-  searchParams: Record<string, string>
-}
+	searchParams: Record<string, string>;
+};
 
 export default async function Home({ searchParams }: HomeProps) {
-  return (
-    <div className="space-y-4 p-4 lg:p-8">
-      <Title>Demos</Title>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-2xl">
-        <ProjectLink
-          name="Color Grid"
-          url="/color-grid"
-          description="Drop different shades of pink into a grid."
-        />
-        <ProjectLink
-          name="To Do List"
-          url="/to-do-list"
-          description="Create and edit items in a to do list."
-        />
-        <ProjectLink
-          name="Text Editor"
-          url="/text-editor"
-          description="A collaborative text editor built on top of the open-source Quill package."
-        />
-        <ProjectLink
-          name="Code Editor"
-          url="/code-editor"
-          description="A collaborative code editor built on top of the open source CodeMirror package."
-        />
-        <ProjectLink
-          name="Tree CRDT"
-          url="/tree-crdt"
-          description="A collaborative tree with reparenting."
-        />
-        <ProjectLink
-          name="Voxel Draw"
-          url="/voxels"
-          description="A collaborative voxel drawing app."
-        />
-      </div>
-    </div>
-  )
+	return (
+		<div className="space-y-4 p-4 lg:p-8">
+			<Title>Demos</Title>
+			<div className="grid max-w-2xl grid-cols-1 gap-4 lg:grid-cols-2">
+				<ProjectLink
+					name="Color Grid"
+					url="/color-grid"
+					description="Drop different shades of pink into a grid."
+				/>
+				<ProjectLink
+					name="To Do List"
+					url="/to-do-list"
+					description="Create and edit items in a to do list."
+				/>
+				<ProjectLink
+					name="Text Editor"
+					url="/text-editor"
+					description="A collaborative text editor built on top of the open-source Quill package."
+				/>
+				<ProjectLink
+					name="Code Editor"
+					url="/code-editor"
+					description="A collaborative code editor built on top of the open source CodeMirror package."
+				/>
+				<ProjectLink
+					name="Tree CRDT"
+					url="/tree-crdt"
+					description="A collaborative tree with reparenting."
+				/>
+				<ProjectLink
+					name="Voxel Draw"
+					url="/voxels"
+					description="A collaborative voxel drawing app."
+				/>
+			</div>
+		</div>
+	);
 }
 
 interface ProjectLinkProps {
-  name: string
-  url: string
-  description: string
+	name: string;
+	url: string;
+	description: string;
 }
 function ProjectLink(props: ProjectLinkProps) {
-  return (
-    <Link
-      href={props.url}
-      className="flex flex-col pt-6 pb-8 px-6 bg-white/50 border border-white/80 shadow-sm hover:bg-white/90 transition-all rounded-lg"
-    >
-      <span className="text-pink-950 font-medium text-base lg:text-lg pb-2">{props.name}</span>
-      <span className="text-pink-950 text-sm">{props.description}</span>
-    </Link>
-  )
+	return (
+		<Link
+			href={props.url}
+			className="flex flex-col rounded-lg border border-white/80 bg-white/50 px-6 pb-8 pt-6 shadow-sm transition-all hover:bg-white/90"
+		>
+			<span className="pb-2 text-base font-medium text-pink-950 lg:text-lg">
+				{props.name}
+			</span>
+			<span className="text-sm text-pink-950">{props.description}</span>
+		</Link>
+	);
 }
